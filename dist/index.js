e669ea483d80671dd91ca6204da74006d8174406
adriantayeh <adriantayeh@gmail.com>
2025-02-26 10:47:40 +0100

    attempting path fix

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,9 @@ import express from "express";
 import cors from "cors";
 import { v4 as uuidv4 } from "uuid";
 import fs from "fs/promises";
-import path from "path";
-import { fileURLToPath } from "url";
 const PORT = 3000;
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const PATH = path.join(__dirname, "bookDB.json");
+const PATH = "./src/bookDB.json";
 app.use(express.json());
 app.use(cors());
 app.listen(PORT, () => {
